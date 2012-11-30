ad088e2ed875e011360e0c262240775ce74f761b
Weston Hanners <weston@hanners.us>
2012-11-30 14:35:42 -0500

    Started working on NSTableView implementation.

--- RSSNotifier/Controllers/NPrefWindowController.h
+++ RSSNotifier/Controllers/NPrefWindowController.h
@@ -8,6 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface NPrefWindowController : NSWindowController
+@interface NPrefWindowController : NSWindowController <NSTableViewDataSource, NSTableViewDelegate>
 
 @end
--- RSSNotifier/Controllers/NPrefWindowController.m
+++ RSSNotifier/Controllers/NPrefWindowController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NPrefWindowController.h"
+#import "NDataStorage.h"
 
 @interface NPrefWindowController ()
 
@@ -31,4 +32,11 @@
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
 
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return [NDataStorage.feeds count];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    return [[NDataStorage.feeds objectAtIndex:row] valueForKey:tableColumn.identifier];
+}
 @end
