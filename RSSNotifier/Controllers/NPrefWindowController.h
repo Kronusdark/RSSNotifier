4ad842db340749a5e85611bf14b28fdbd3e33cb3
Weston Hanners <weston@hanners.us>
2012-11-30 14:14:21 -0500

    Added window controller.

---
+++ RSSNotifier/Controllers/NPrefWindowController.h
@@ -0,0 +1,13 @@
+//
+//  NPrefWindowController.h
+//  RSSNotifier
+//
+//  Created by Weston (Work) on 11/30/12.
+//  Copyright (c) 2012 Weston Hanners. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NPrefWindowController : NSWindowController
+
+@end
---
+++ RSSNotifier/Controllers/NPrefWindowController.m
@@ -0,0 +1,34 @@
+//
+//  NPrefWindowController.m
+//  RSSNotifier
+//
+//  Created by Weston (Work) on 11/30/12.
+//  Copyright (c) 2012 Weston Hanners. All rights reserved.
+//
+
+#import "NPrefWindowController.h"
+
+@interface NPrefWindowController ()
+
+@end
+
+@implementation NPrefWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
