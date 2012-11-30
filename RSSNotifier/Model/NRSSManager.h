ffd903e1e0c5478959d72f6ceebb683795aab347
Weston Hanners <weston@hanners.us>
2012-11-30 14:14:56 -0500

    Added RSS data controller.

---
+++ RSSNotifier/Model/NRSSManager.h
@@ -0,0 +1,13 @@
+//
+//  NRSSManager.h
+//  RSSNotifier
+//
+//  Created by Weston (Work) on 11/30/12.
+//  Copyright (c) 2012 Weston Hanners. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NRSSManager : NSObject
+
+@end
---
+++ RSSNotifier/Model/NRSSManager.m
@@ -0,0 +1,13 @@
+//
+//  NRSSManager.m
+//  RSSNotifier
+//
+//  Created by Weston (Work) on 11/30/12.
+//  Copyright (c) 2012 Weston Hanners. All rights reserved.
+//
+
+#import "NRSSManager.h"
+
+@implementation NRSSManager
+
+@end
