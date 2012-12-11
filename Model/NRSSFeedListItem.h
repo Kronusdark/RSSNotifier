62369e98914455639bc6bb829b057cf033fd008b
Weston Hanners <weston@hanners.us>
2012-12-11 13:17:31 -0500

    Added new properties to NRSSFeedListItem for storing time and URL info.

--- Model/NRSSFeedListItem.h
+++ Model/NRSSFeedListItem.h
@@ -11,6 +11,9 @@
 @interface NRSSFeedListItem : NSObject <NSCoding>
 
 @property (strong, nonatomic) NSString *url;
+@property (strong, nonatomic) NSString *lastChecked;
+@property (strong, nonatomic) NSString *savedURL;
 @property (strong, nonatomic) NSString *title;
-
+@property (strong, nonatomic) NSString *latestUID;
+@property (strong, nonatomic) RSSFeed *cachedFeed;
 @end
--- Model/NRSSFeedListItem.m
+++ Model/NRSSFeedListItem.m
@@ -13,14 +13,22 @@
 - (id)initWithCoder:(NSCoder *)aDecoder {
     if (self = [super init]) {
         self.url = [aDecoder decodeObjectForKey:@"url"];
+        self.lastChecked = [aDecoder decodeObjectForKey:@"lastChecked"];
+        self.savedURL = [aDecoder decodeObjectForKey:@"savedURL"];
         self.title = [aDecoder decodeObjectForKey:@"title"];
+        self.latestUID = [aDecoder decodeObjectForKey:@"latestUID"];
+        self.cachedFeed = [aDecoder decodeObjectForKey:@"cachedFeed"];
     }
     return self;
 }
 
 - (void)encodeWithCoder:(NSCoder *)aCoder {
     [aCoder encodeObject:self.url forKey:@"url"];
+    [aCoder encodeObject:self.lastChecked forKey:@"lastChecked"];
+    [aCoder encodeObject:self.savedURL forKey:@"savedURL"];
     [aCoder encodeObject:self.title forKey:@"title"];
+    [aCoder encodeObject:self.latestUID forKey:@"latestUID"];
+    [aCoder encodeObject:self.cachedFeed forKey:@"cachedFeed"];
 }
 
 @end
